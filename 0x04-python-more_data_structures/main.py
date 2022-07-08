7b555e2045d5f3315e14cdee143d9c7efa2aaaaf
bkablam11 <bkablam11@gmail.com>
2022-07-08 01:56:25 -0700

    adding

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
+
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)#!/usr/bin/python3
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
