d1cee5d5604db0e96dfdec414a4630930dc12a9a
bkablam11 <bkablam11@gmail.com>
2022-09-06 17:58:12 -0700

    adding

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,4 @@
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
